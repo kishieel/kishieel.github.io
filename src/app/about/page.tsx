87aa967c608a04c3dbc5d6dc2a9865e4e89978a5
Tomasz Kisiel <tkisiel5w4@yahoo.com>
2023-11-24 17:32:49 +0100

    feat: resume file & mock navigation

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import Navigation from "@src/components/navigation";
+
+export default function About() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Navigation/>
+        </main>
+    )
+}
